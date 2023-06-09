550b963728dc7e9b3cfc8f40e4b1ed5ccce7c97f
xneilmacabulos@gmail.com <xneilmacabulos@gmail.com>
2023-06-09 11:19:51 +0800

    FinalsExam2ndExam

---
+++ ECommerce.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Contracts.Categories;
+using ECommerce.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.WebSockets;
+
+namespace NeilMacabulos.Finals_.Pages
+{
+    public class Index : PageModel
+    {
+        private readonly ILogger<Index> _logger;
+        private readonly ICategoriesService _categoriesService;
+        public Index(ILogger<Index> logger, ICategoriesService categoriesService)
+        {
+            _logger = logger;
+            _categoriesService = categoriesService;
+        }
+
+        public void OnGet()
+        {
+            this.Categories = _categoriesService.GetAll();
+        }
+
+        public List<CategoriesDto> Categories { get; set; }
+        public class ViewModel
+        {
+            public CategoriesDto? Categories { get; set; }
+        }
+
+    }
+}
